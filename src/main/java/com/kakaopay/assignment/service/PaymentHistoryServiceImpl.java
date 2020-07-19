01e1f1eddfc41c11a998cfe443de53c0f076e0f2
박동엽 <ninjasul@onestorecorp.com>
2020-07-19 11:52:20 +0900

    @Transactional 경로 수정

--- src/main/java/com/kakaopay/assignment/service/PaymentHistoryServiceImpl.java
+++ src/main/java/com/kakaopay/assignment/service/PaymentHistoryServiceImpl.java
@@ -12,9 +12,9 @@ import com.kakaopay.assignment.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import javax.transaction.Transactional;
 import java.util.Objects;
 import java.util.Optional;
 
